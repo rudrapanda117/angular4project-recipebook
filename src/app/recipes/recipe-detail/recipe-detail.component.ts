7968b6b94848a68fe589c75947c8485b6b6585db
Rudrasish <rudrapanda117@gmail.com>
2017-12-27 13:59:29 +0530

    Configuring route parameters

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,11 +9,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
- @Input('recipe') recipe: Recipe;
+ recipe: Recipe;
+ id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    //const id = +this.route.snapshot.params['id'];
+
+    this.route.paramMap.subscribe(
+      (params: Params) => {
+        this.id = +params.get('id');
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
   }
 
   onAddToShoppingList() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,41 +1,52 @@
-import { ShoppingListService } from './../shopping-list/shopping-list.service';
-import { EventEmitter, Injectable } from '@angular/core';
-import { Recipe } from './recipe.model';
-import { Ingredient } from '../shared/ingredient.model';
+import {
+  ShoppingListService
+} from './../shopping-list/shopping-list.service';
+import {
+  EventEmitter,
+  Injectable
+} from '@angular/core';
+import {
+  Recipe
+} from './recipe.model';
+import {
+  Ingredient
+} from '../shared/ingredient.model';
 
 
 @Injectable()
 export class RecipeService {
 
-  constructor(private shoppingListService: ShoppingListService){}
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  public recipeSelected = new EventEmitter<Recipe>();
+  public recipeSelected = new EventEmitter < Recipe > ();
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
         new Ingredient('Meat', 1),
         new Ingredient('Fries', 21),
         new Ingredient('Meater', 1),
         new Ingredient('fishFries', 21)
-    ]),
+      ]),
     new Recipe('Sea Food recipe', 'This is simply a test',
-      'https://myrecipehampers.files.wordpress.com/2013/04/mrh-square-logo.png',
-    [
-      new Ingredient('DeliMeat', 1),
-      new Ingredient('Fries', 21),
-      new Ingredient('Crab', 1),
-      new Ingredient('loadster', 21),
-    ])
+      'https://myrecipehampers.files.wordpress.com/2013/04/mrh-square-logo.png', [
+        new Ingredient('DeliMeat', 1),
+        new Ingredient('Fries', 21),
+        new Ingredient('Crab', 1),
+        new Ingredient('loadster', 21),
+      ])
   ];
 
-     public getRecipes() {
-          return this.recipes.slice();
-      }
+  public getRecipes() {
+    return this.recipes.slice();
+  }
 
-      public addIngredientForShoppingList(ingredients: Ingredient[]) {
-          this.shoppingListService.addIngredients(ingredients);
-      }
+  public addIngredientForShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  public getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
 }
